4adfd290dfcf11fa9748501ca99699448ec5335d
Julian Przedwojski <julian95przedwojski@gmail.com>
2024-06-02 21:37:48 +0200

    mock provider in random search test

--- app/tests/Services/Strategy/RandomSearchStrategyTest.php
+++ app/tests/Services/Strategy/RandomSearchStrategyTest.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace tests\Services\Strategy;
 
-use App\Services\Provider\MoviesProvider;
+use App\Services\Provider\MoviesProviderInterface;
 use App\Services\Strategy\RandomSearchStrategy;
 use PHPUnit\Framework\TestCase;
 
 class RandomSearchStrategyTest extends TestCase
 {
-    public function test_execute(): void
+    /**
+     * @param string[] $movies
+     * @dataProvider provider
+     */
+    public function test_execute(array $movies): void
     {
-        $testClass = new RandomSearchStrategy(new MoviesProvider());
+        $mockedMoviesProvider = $this->createMock(MoviesProviderInterface::class);
+        $mockedMoviesProvider->method('provide')->willReturn($movies);
+
+        $testClass = new RandomSearchStrategy($mockedMoviesProvider);
 
         $result = $testClass->execute();
 
@@ -21,5 +28,36 @@ class RandomSearchStrategyTest extends TestCase
         foreach ($result as $filmName) {
             $this->assertIsString($filmName);
         }
+    }
+
+    public function provider(): array
+    {
+        return [
+            [
+                [
+                    "Leon zawodowiec",
+                    "Nietykalni",
+                    "Władca Pierścieni: Powrót króla",
+                    "Chłopaki nie płaczą",
+                    "Człowiek z blizną",
+                    "Pianista",
+                    "Doktor Strange",
+                    "Szeregowiec Ryan"
+                ]
+            ],
+            [
+                [
+                    "Wyspa tajemnic",
+                    "Django",
+                    "American Beauty",
+                    "Szósty zmysł",
+                    "Gwiezdne wojny: Nowa nadzieja",
+                    "Mroczny Rycerz",
+                    "Władca Pierścieni: Drużyna Pierścienia",
+                    "Harry Potter i Kamień Filozoficzny",
+                    "Green Mile"
+                ]
+            ],
+        ];
     }
 }
